dbe9c6629547d3ab14c63687f639db4f498abd47
duenker <duenker@6fa1b897-c327-0410-b655-c6e5446379c1>
2007-02-11 16:26:05 +0000

    Documentation Mainfile

--- oraload/main.cpp
+++ oraload/main.cpp
@@ -66,6 +66,9 @@
  *
  * - note that you always have to lock the row with "for update", if you upload something. You do not 
  *   need that when downloading.
+ * 
+ * \subsubsection Example with EasyConnect (e.g. if you have InstantClient and no TNSNAMES.ora)
+ * oraload hr hr //lynx:1521/xe DC "select blob_field from blob_table where pk_field = 4711" music.mp3 ./log.txt
  *
  * \subsection Using Using the library
  * You can use the library to implement in your own programs. A good starting point is the test-directory
